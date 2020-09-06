d5dbd32884a7ab37a55794339b4f62a1c133215e
Pierre-Yves Fourmond <pyfourmond@gmail.com>
2020-09-06 09:56:26 +0200

    Remove unused import

--- src/test/java/org/grumpyf0x48/liar/update/SoftwareUpdateServiceTest.java
+++ src/test/java/org/grumpyf0x48/liar/update/SoftwareUpdateServiceTest.java
@@ -11,7 +11,6 @@ import org.junit.Test;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Properties;
 
 import static org.apache.commons.io.FileUtils.contentEquals;
