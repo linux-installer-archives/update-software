9b3fc53ae9e635fc8672a7cdd54dc6c0547ef2af
Pierre-Yves Fourmond <pyfourmond@gmail.com>
2020-12-31 10:55:16 +0100

    Add cloc and hexa

--- src/test/java/org/grumpyf0x48/liar/update/SoftwareUpdateServiceTest.java
+++ src/test/java/org/grumpyf0x48/liar/update/SoftwareUpdateServiceTest.java
@@ -81,7 +81,7 @@ public class SoftwareUpdateServiceTest
     public void getSoftwareListTest() throws IOException
     {
         final String[] softwareList = nextUpdateService.getSoftwareList();
-        Assert.assertEquals(70, softwareList.length);
+        Assert.assertEquals(72, softwareList.length);
     }
 
     @Test
@@ -247,7 +247,7 @@ public class SoftwareUpdateServiceTest
                 Assert.fail(software + " is not updatable: " + e.getMessage());
             }
         }
-        Assert.assertEquals("Bad number of updatable software", 44, count);
+        Assert.assertEquals("Bad number of updatable software", 46, count);
     }
 
     private static SoftwareUpdateService getSoftwareUpdateService()
